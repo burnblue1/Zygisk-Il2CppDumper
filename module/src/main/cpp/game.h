06a42a25a3ba7c9a0b795f70896bfa7625002a27
burnblue1 <burnblue@qq.com>
2026-05-13 00:31:21 +0800

    game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -1,10 +1,8 @@
 //
 // Created by Perfare on 2020/7/4.
 //
-
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
-
-#define GamePackageName "com.game.packagename"
-
+#define GamePackageName "com.gof.china"
+#define GameLibName "libil2cpp.so"
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
